3f4bb00f91126d1ea36df8d2e581269cecb4fa1f
rtds1103 <ganesh14311@gmail.com>
2020-07-17 12:31:30 +0530

    upload5

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,15 +10,14 @@ $(function () {
       var name = $("input#name").val();
       var email = $("input#email").val();
       var message = $("textarea#message").val();
-      console.log(message);
+
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
       if (firstName.indexOf(" ") >= 0) {
         firstName = name.split(" ").slice(0, -1).join(" ");
       }
       $.ajax({
-        url:
-          "https://jddesign.000webhostapp.com/js/contact_me.php/mail/contact_me.php",
+        url: "../mail/contact_me.php",
         type: "POST",
         data: {
           name: name,
@@ -78,3 +77,24 @@ $(function () {
 $("#name").focus(function () {
   $("#success").html("");
 });
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
