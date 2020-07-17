ba609c396379feb541c9e9b4c65e0cbc5520eea5
rtds1103 <ganesh14311@gmail.com>
2020-07-17 11:01:13 +0530

    any essage

---
+++ mail/contact_me.php
@@ -0,0 +1,17 @@
+
+
+<?php
+     if (isset($_POST['submit'])) {
+        $name = $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $message = $_REQUEST['message'];
+
+      // Set your email address where you want to receive emails. 
+       $to = 'ganesh14311@gmail.com';
+       $subject = 'Contact Request From Website';
+       $headers = "From: ".$name." <".$email."> \r\n";
+       $send_email = mail($to,$subject,$message,$headers);
+
+       echo ($send_email) ? 'success' : 'error';
+
+  }?>
