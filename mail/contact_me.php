f524f286f3c6c491bb6f1b23e54098cef1e60ac2
rtds1103 <34368753+rtds1103@users.noreply.github.com>
2020-07-17 11:06:57 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,17 +1,180 @@
 
 
+
 <?php
-     if (isset($_POST['submit'])) {
-        $name = $_REQUEST['name'];
-        $email = $_REQUEST['email'];
-        $message = $_REQUEST['message'];
+  if ($_POST['name'] !=" ") {
 
-      // Set your email address where you want to receive emails. 
-       $to = 'ganesh14311@gmail.com';
-       $subject = 'Contact Request From Website';
-       $headers = "From: ".$name." <".$email."> \r\n";
-       $send_email = mail($to,$subject,$message,$headers);
+$to      = 'ganesh14311@gmail.com';
+$subject = 'Enquiry From Customer';
+$message = '
+<html>
+<head>
+<title>HTML email</title>
+<style>
+/***
+User Profile Sidebar by @keenthemes
+A component of Metronic Theme - #1 Selling Bootstrap 3 Admin Theme in Themeforest: http://j.mp/metronictheme
+Licensed under MIT
+***/
 
-       echo ($send_email) ? 'success' : 'error';
+body {
+    padding: 0;
+    margin: 0;
+}
 
-  }?>
+html { -webkit-text-size-adjust:none; -ms-text-size-adjust: none;}
+@media only screen and (max-device-width: 680px), only screen and (max-width: 680px) { 
+    *[class="table_width_100"] {
+		width: 96% !important;
+	}
+	*[class="border-right_mob"] {
+		border-right: 1px solid #dddddd;
+	}
+	*[class="mob_100"] {
+		width: 100% !important;
+	}
+	*[class="mob_center"] {
+		text-align: center !important;
+	}
+	*[class="mob_center_bl"] {
+		float: none !important;
+		display: block !important;
+		margin: 0px auto;
+	}	
+	.iage_footer a {
+		text-decoration: none;
+		color: #929ca8;
+	}
+	img.mob_display_none {
+		width: 0px !important;
+		height: 0px !important;
+		display: none !important;
+	}
+	img.mob_width_50 {
+		width: 40% !important;
+		height: auto !important;
+	}
+}
+.table_width_100 {
+	width: 680px;
+}
+</style>
+</head>
+<body>
+<div id="mailsub" class="notification" align="center">
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0" style="min-width: 320px;"><tr><td align="center" bgcolor="#eff3f8">
+
+
+<!--[if gte mso 10]>
+<table width="680" border="0" cellspacing="0" cellpadding="0">
+<tr><td>
+<![endif]-->
+
+<table border="0" cellspacing="0" cellpadding="0" class="table_width_100" width="100%" style="max-width: 680px; min-width: 300px;">
+    <tr><td>
+	<!-- padding -->
+	</td></tr>
+	<!--header -->
+	<tr><td align="center" bgcolor="#ffffff">
+		<!-- padding -->
+		<table width="90%" border="0" cellspacing="0" cellpadding="0"><div style="height: 30px; line-height: 30px; font-size: 10px;"></div>
+			<tr><td align="center">
+			    		<a href="#" target="_blank" style="color: #596167; font-family: Arial, Helvetica, sans-serif; float:left; width:100%; padding:20px;text-align:center; font-size: 13px;">
+									<font face="Arial, Helvetica, sans-seri; font-size: 13px;" size="3" color="#596167">
+									<img src="http://dev.yogabar.sg/web/images/img_logo.png" width="120" alt="Metronic" border="0"  /></font></a>
+					</td>
+					<td align="right">
+				<!--[endif]--><!-- 
+
+			</td>
+			</tr>
+		</table>
+		<!-- padding -->
+	</td></tr>
+	<!--header END-->
+
+	<!--content 1 -->
+	<tr><td align="center" bgcolor="#fbfcfd">
+		<table width="90%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td align="center">
+				<!-- padding -->
+				<div style="line-height: 44px;">
+					<font face="Arial, Helvetica, sans-serif" size="5" color="#57697e" style="font-size: 34px;">
+					<span style="font-family: Arial, Helvetica, sans-serif; font-size: 34px; color: #57697e;">
+						Hi Dream Homz
+					</span></font>
+				</div>
+				<!-- padding --><div style="height: 10px; line-height: 10px; font-size: 10px;"></div>
+			</td></tr>
+			<tr><td align="center">
+				<div style="line-height: 24px;">
+					<font face="Arial, Helvetica, sans-serif" size="4" color="#57697e" style="font-size: 15px;">
+					<span style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; color: #57697e;">
+						You Have Received a Mail For an Enquiry
+					</span></font>
+				</div>
+				<!-- padding --><div style="height: 20px; line-height: 20px; font-size: 10px;"></div>
+			</td></tr>
+			<tr><td align="center">
+				<div style="line-height: 33px;">
+                    <font face="Arial, Helvetica, sans-serif" size="4" color="#57697e" style="font-size: 15px;">
+                    <div style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; color: #57697e;">
+                         <span>Name :</span><span>'. $_POST['name'].'<br/></span>
+                         </div>
+                          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; color: #57697e;">
+                         <span>Email:</span> <span>'. $_POST['email'].'<br/></span>
+                         </div>
+                          <div style="font-family: Arial, Helvetica, sans-serif; font-size: 15px; color: #57697e;">
+                         <span>Message :</span><span>'. $_POST['message'].'<br/></span>
+                    </div></font>
+				</div>
+				<!-- padding --><div style="height: 13px; line-height: 3px; font-size: 10px;"></div>
+			</td></tr>
+		</table>		
+	</td></tr>
+	<!--content 1 END-->
+
+
+	<!--footer -->
+	<tr><td class="iage_footer" align="center" bgcolor="#ffffff">
+
+		
+		<table width="100%" border="0" cellspacing="0" cellpadding="0">
+			<tr><td align="center" style="padding:20px;flaot:left;width:100%; text-align:center;">
+				<font face="Arial, Helvetica, sans-serif" size="3" color="#96a5b5" style="font-size: 13px;">
+				<span style="font-family: Arial, Helvetica, sans-serif; font-size: 13px; color: #96a5b5;">
+					2020 © Dream Homz. ALL Rights Reserved.
+				</span></font>				
+			</td></tr>			
+		</table>
+		
+
+	</td></tr>
+	<!--footer END-->
+	<tr><td>
+
+	</td></tr>
+</table>
+<!--[if gte mso 10]>
+</td></tr>
+</table>
+<![endif]-->
+ 
+</td></tr>
+</table>
+</body>
+</html>
+';
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$headers .= 'From: '. $_POST['email'].'' . "\r\n" .
+    'Reply-To: '. $_POST['email'].'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+}
+?>
