d150adc8845d5439ab9de3d85a3a345c9dd3bd05
rtds1103 <ganesh14311@gmail.com>
2020-07-20 17:09:09 +0530

    commit new y website

--- js/contact_me.js
+++ js/contact_me.js
@@ -11,23 +11,20 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var message = $("textarea#message").val();
-           
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://jddesign.000webhostapp.com/mail/contact_me.php",
+                url: "././mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
                     message: message
                 },
-
                 cache: false,
-
                 success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
@@ -43,7 +40,12 @@ $(function() {
                 },
                 error: function() {
                     // Fail message
-                  
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
                     $('#contactForm').trigger("reset");
                 },
             })
@@ -64,22 +66,3 @@ $(function() {
 $('#name').focus(function() {
     $('#success').html('');
 });
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
